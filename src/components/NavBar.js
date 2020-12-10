18cb5af0eb6f6fa6314d702510c27062e3b41b93
geraldy1st <geraldy.leondas@gmail.com>
2020-12-11 00:45:38 +0100

    import social-icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,66 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
-    return (
-        <header className='bg-blue-200'>
-            <div className='container mx-auto flex justify-between'>
-                <nav className="flex">
-                    <NavLink to='/' exact activeClassName='text-white' className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
-                        Geraldy
-                    </NavLink>
-                    <NavLink to='/post'>
-                        Blog Posts
-                    </NavLink>
-                    <NavLink to='/project'>
-                        Projects
-                    </NavLink>
-                    <NavLink to='/about'>
-                        About Me!
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="bg-red-500">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Geraldy
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
+          >
+            About Me!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://twitter.com/Geraldy_Leondas"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/generald/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/geraldy1st"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
