f24604599fc36db432b9a7ff2567f649ec44c4ed
geraldy1st <geraldy.leondas@gmail.com>
2020-12-11 13:41:36 +0100

    edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import image from '../images/tokyo.jpg'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home Page!</h1>
-        </div>
+        <main>
+            <img src={image} alt="Tokyo" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-3xl text-green-100 cursive leading-none lg:leading-snug home-name text-shadow">Hey, I'm Geraldy</h1>
+            </section>
+        </main>
     )
 }
